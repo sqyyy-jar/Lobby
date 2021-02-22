c1211e2e563af0afc47e1782d7292f9f4a441766
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-02-22 18:11:22 +0100

    Update EventChangeGameMode.java

--- src/main/java/net/craftions/lobby/events/EventChangeGameMode.java
+++ src/main/java/net/craftions/lobby/events/EventChangeGameMode.java
@@ -1,5 +1,7 @@
 package net.craftions.lobby.events;
 
+import net.craftions.lobby.Lobby;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,7 +11,12 @@ public class EventChangeGameMode implements Listener {
     @EventHandler
     public void onChangeGameMode(PlayerGameModeChangeEvent e) {
         if (e.getNewGameMode().equals(GameMode.ADVENTURE) || e.getNewGameMode().equals(GameMode.SURVIVAL)) {
-            e.getPlayer().setAllowFlight(true);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Lobby.getPlugin(Lobby.class), new Runnable() {
+                @Override
+                public void run() {
+                    e.getPlayer().setAllowFlight(true);
+                }
+            }, 0);
         }
     }
 }
